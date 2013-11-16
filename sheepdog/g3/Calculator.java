4f9ace9b49d251c08876568731f319fa9a35497a
Jonathan Balsano <jrbalsano@gmail.com>
2013-11-16 14:00:57 -0500

    Added basic calculator functions

---
+++ sheepdog/g3/Calculator.java
@@ -0,0 +1,73 @@
+package sheepdog.g3;
+
+import java.util.ArrayList;
+
+import sheepdog.sim.Point;
+
+public class Calculator {
+  public static final double RUN_DISTANCE = 2.0;
+  public static final double WALK_DISTANCE = 10.0;
+  public static final double FIELD_SIZE = 100.0;
+  public static final double DOG_MAX_SPEED = 2.0;
+  public static final double OPEN_LEFT = 49.0;
+  public static final double OPEN_RIGHT = 51.0;
+  
+  public static double dist(Point p1, Point p2) {
+    return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+  }
+  
+  public static boolean withinRunDistance(Point sheep, Point position) {
+    return dist(sheep, position) < RUN_DISTANCE;
+  }
+  
+  public static boolean withinWalkDistance(Point sheep, Point position) {
+    return dist(sheep, position) < WALK_DISTANCE;
+  }
+  
+  public static boolean isMoveValid(Point src, Point dst) {
+      if (dst.x < 0 || dst.x > FIELD_SIZE) {
+          return false;
+      }
+      if (dst.y < 0 || dst.y > FIELD_SIZE) {
+          return false;
+      }
+      if (dist(src, dst) > DOG_MAX_SPEED) {
+          return false;
+      }
+      if (hitTheFence(src, dst)) {
+          return false;
+      }
+      return true;
+  }
+  
+  public static int getSide(double x) {
+    if (x < FIELD_SIZE * 0.5)
+      return 0;
+    else if (x > FIELD_SIZE * 0.5)
+      return 1;
+    else
+      return 2;
+}
+
+  public static boolean hitTheFence(Point src, Point dst) {
+    // on the same side
+    if (getSide(src.x) == getSide(dst.x)) { return false; }
+
+    // one point is on the fence
+    if (getSide(src.x) == 2 || getSide(dst.x) == 2) { return false; }
+    
+    // Find the y coordinate of the the point (50, y) on the line connecting
+    // the two points.
+    double y3 = (dst.y-src.y)/(dst.x-src.x)*(50-src.x)+src.y;
+
+    // between the opening?
+    if (y3 >= OPEN_LEFT && y3 <= OPEN_RIGHT) {
+        return false;
+    }
+    else {
+        return true;
+    }
+  }
+  
+  
+}
