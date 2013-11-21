fc053fc0e44d99d532061e5bd5208803b4443e20
Jonathan Balsano <jrbalsano@gmail.com>
2013-11-21 00:59:53 -0500

    Updated to fix gap conflict problems

--- sheepdog/g3/StraightLineBrainGap.java
+++ sheepdog/g3/StraightLineBrainGap.java
@@ -12,8 +12,36 @@ public class StraightLineBrainGap extends StraightLineBrain {
 
     @Override
     protected int chooseSheep(Point[] dogs, Point[] sheeps, ArrayList<Integer> undeliveredIndices) {
+        int chosenSheepIndex = -1;
         ArrayList<Integer> sortedIndices = getDistanceSortedIndices(sheeps, GAP, undeliveredIndices);
-        return sortedIndices.get(Math.min(mId, sortedIndices.size()-1));   
+        ArrayList<Integer> dogDistances[] = new ArrayList[dogs.length];
+        dogDistances[0] = sortedIndices;
+        ArrayList<Integer> dogIndices = new ArrayList<Integer>(dogs.length);
+        for (int i = 0; i < dogs.length; i++) { dogIndices.add(i); }
+        
+        for (int i : sortedIndices) {
+            getDistanceSortedIndices(dogs, sheeps[i], dogIndices);
+            int bestDog = dogIndices.get(0); 
+            if (bestDog == mId) {
+                chosenSheepIndex = i;
+                break;
+            }
+            else {
+                if (dogDistances[bestDog] == null) {
+                    dogDistances[bestDog] = getDistanceSortedIndices(sheeps, dogs[bestDog], undeliveredIndices);
+                }
+                if (dogDistances[bestDog].indexOf(i) > bestDog) {
+                    chosenSheepIndex = i;
+                    break;
+                }
+            }
+        }
+        
+        if (chosenSheepIndex == -1) {
+            chosenSheepIndex = sortedIndices.get(0);
+        }
+        
+        return chosenSheepIndex;   
     }
 
 }
