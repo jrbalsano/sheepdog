e1cd703c1dd63d2376780fa7ffaa8fc6ae18c2b7
yash <yash@ubuntu.(none)>
2013-12-02 02:30:35 -0500

    Added SweeperBrain

--- sheepdog/g3/Player.java
+++ sheepdog/g3/Player.java
@@ -1,12 +1,15 @@
 package sheepdog.g3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+
 import sheepdog.sim.Point;
 
 public class Player extends sheepdog.sim.Player  {
   private int mNblacks;
   private boolean mMode;
   private DogBrain mBrain;
+  static boolean sweeperComplete = false;
   
   @Override
   public void init(int nblacks, boolean mode) {
@@ -16,16 +19,27 @@ public class Player extends sheepdog.sim.Player  {
 //    mBrain = new StraightLineBrainFar(id, mode, nblacks);
 //    mBrain = new StraightLineBrainMe(id, mode, nblacks);
 //    mBrain = new SteinerBrain(id, mode, nblacks);
-    mBrain = new ConvexHullBrain(id, mode, nblacks);
+//    mBrain = new ConvexHullBrain(id, mode, nblacks);
   }
 
   @Override
   public Point move(Point[] dogs, Point[] sheeps) {
 	  
+	  ArrayList<Integer> undeliveredIndices = Calculator.undeliveredBlackSheep(sheeps);
+	  System.out.println("Number of Undelivered Sheep: "+undeliveredIndices.size());
+	  if(mMode || sweeperComplete || dogs.length < 26)
+//		  if(dogs.length>=10 && !mMode)
+//			  mBrain = new ConvexHullBrain(id, mMode, mNblacks);
+//		  else
+			  mBrain = new StraightLineBrainMe(id, mMode, mNblacks);
+
+	  else
+		  mBrain = new SweeperBrain(id, mMode, mNblacks);
+	  
 //	  if(SteinerBrain.removal==1)
 //		  mBrain = new StraightLineBrainMe(id, mMode, mNblacks);
 		  
-	   
+	  
     return mBrain.getMove(dogs, sheeps);
   }
 
